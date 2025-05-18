322fc9e7f01bdd0ffe1239f0e616eca799498697
Julian Kliebhan <hallo@jkliebhan.de>
2025-05-18 16:54:59 +0200

    Input and Preview Markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef, useState } from "react";
+import "./App.css";
+import { marked } from "marked";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [markdown, setMarkdown] = useState("");
+  const inputRef = useRef<HTMLDivElement | null>(null);
+
+  marked.setOptions({
+    breaks: true, //  Enable line breaks with single newline
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMarkdown(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+    // if (event.key === "Enter") {
+    //   event.preventDefault();
+    // }
+  };
+
+  const renderMarkdown = (markdown: string) => {
+    try {
+      console.log(marked(markdown));
+      return { __html: marked(markdown) };
+    } catch (e) {
+      console.error("Error parsing Markdown: ", e);
+      return { __html: "<p>Error parsing Markdown</p>" };
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main">
+        <h1>Markdown Together</h1>
+        <div id="doc" className="standard">
+          <textarea
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+          ></textarea>
+        </div>
+        <div
+          ref={inputRef}
+          id="prev"
+          className="standard"
+          dangerouslySetInnerHTML={renderMarkdown(markdown)}
+        ></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
